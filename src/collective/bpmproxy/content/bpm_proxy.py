d0c25437c87d4ec8583c45ef56b66e6300db417e
Asko Soukka <asko.soukka@iki.fi>
2022-05-13 12:36:50 +0200

    "Add view: view"

--- src/collective/bpmproxy/content/bpm_proxy.py
+++ src/collective/bpmproxy/content/bpm_proxy.py
@@ -1,17 +1,13 @@
 # -*- coding: utf-8 -*-
 # from plone.app.textfield import RichText
 # from plone.autoform import directives
+from collective.bpmproxy import _
 from plone.dexterity.content import Item
 # from plone.namedfile import field as namedfile
 from plone.supermodel import model
-
-
 # from plone.supermodel.directives import fieldset
 # from z3c.form.browser.radio import RadioFieldWidget
 from zope import schema
-
-
-from collective.bpmproxy import _
 
 
 class IBpmProxy(model.Schema):
---
+++ src/collective/bpmproxy/tests/test_view_view.py
@@ -0,0 +1,49 @@
+# -*- coding: utf-8 -*-
+from collective.bpmproxy.testing import (
+    COLLECTIVE_BPMPROXY_FUNCTIONAL_TESTING,
+    COLLECTIVE_BPMPROXY_INTEGRATION_TESTING,
+)
+from plone import api
+from plone.app.testing import setRoles, TEST_USER_ID
+from zope.component import getMultiAdapter
+from zope.component.interfaces import ComponentLookupError
+
+import unittest
+
+
+class ViewsIntegrationTest(unittest.TestCase):
+
+    layer = COLLECTIVE_BPMPROXY_INTEGRATION_TESTING
+
+    def setUp(self):
+        self.portal = self.layer['portal']
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
+        api.content.create(self.portal, 'Folder', 'other-folder')
+        api.content.create(self.portal, 'Document', 'front-page')
+
+    def test_view_is_registered(self):
+        view = getMultiAdapter(
+            (self.portal['other-folder'], self.portal.REQUEST),
+            name='view'
+        )
+        self.assertTrue(view.__name__ == 'view')
+        # self.assertTrue(
+        #     'Sample View' in view(),
+        #     'Sample View is not found in view'
+        # )
+
+    def test_view_not_matching_interface(self):
+        with self.assertRaises(ComponentLookupError):
+            getMultiAdapter(
+                (self.portal['front-page'], self.portal.REQUEST),
+                name='view'
+            )
+
+
+class ViewsFunctionalTest(unittest.TestCase):
+
+    layer = COLLECTIVE_BPMPROXY_FUNCTIONAL_TESTING
+
+    def setUp(self):
+        self.portal = self.layer['portal']
+        setRoles(self.portal, TEST_USER_ID, ['Manager'])
